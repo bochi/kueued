5097dee9344f557bdef0020c4d510287c3bd7132
Stefan Bogner <sbogner@gmail.com>
2012-02-13 10:59:02 +0100

    kueuedsrv

---
+++ srv/kueuedsrv.h
@@ -0,0 +1,56 @@
+/*
+              kueued - create xml data for kueue's qmon 
+              (C) 2012 Stefan Bogner <sbogner@suse.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the
+
+    Free Software Foundation, Inc.
+    59 Temple Place - Suite 330
+    Boston, MA  02111-1307, USA
+
+    Have a lot of fun :-)
+
+*/
+
+#ifndef KUEUEDSRV_H
+#define KUEUEDSRV_H
+
+#include "database.h"
+#include "qtservice/qtservice.h"
+#include "server.h"
+
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+
+class Database;
+class Server;
+
+class KueuedSrv : public QObject
+ {
+     Q_OBJECT
+     
+ public:
+     KueuedSrv();
+     ~KueuedSrv();
+
+    private:
+        Server *mServer;
+        Database* mDB;
+        
+};
+ 
+#endif
