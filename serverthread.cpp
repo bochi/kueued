5793de4cc91536522ec758d36a082909e6cd15ec
Stefan Bogner <sbogner@gmail.com>
2012-09-13 11:28:49 +0200

    srnums

--- serverthread.cpp
+++ serverthread.cpp
@@ -29,7 +29,6 @@
 #include "serverthread.h"
 #include "database.h"
 #include "network.h"
-#include "../google-breakpad/src/testing/gtest/samples/sample3-inl.h"
 
 #include <iostream>
 #include <QtSql>
