61a8d471a9e117ca07a59ba1c804371e1f64ecf4
Stefan Bogner <sbogner@gmail.com>
2013-10-06 17:38:38 +0200

    fix cdata

--- database.cpp
+++ database.cpp
@@ -549,7 +549,7 @@ q+=(                "  and srx.row_id = sr.row_id"
             i.high_value = false;
         }
         
-        i.detailed_desc = query.value( 24 ).toString().replace( "]]>", "]]&gt;" ).replace("\e", "");
+        i.detailed_desc = query.value( 24 ).toString().replace( "]]>", "]]&gt;" ).replace("\\e", "");
         i.alt_contact = query.value( 25 ).toString();
         i.bugId = query.value( 26 ).toString();
         i.cstNum = query.value( 27 ).toString();
