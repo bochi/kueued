151d47db420a0f117ada54ca927fd57d3f5ff09d
Stefan Bogner <sbogner@gmail.com>
2013-10-06 20:07:32 +0200

    fix cdata

--- database.cpp
+++ database.cpp
@@ -529,7 +529,7 @@ q+=(                "  and srx.row_id = sr.row_id"
         }
             
         i.service_level = query.value( 10 ).toInt();
-        i.brief_desc = query.value( 11 ).toString().replace( "]]>", "]]&gt;" ).toAscii().toHex();
+        i.brief_desc = query.value( 11 ).toString().replace( "]]>", "]]&gt;" );
         
         if ( query.value( 12 ).toString() == "Y" )
         {
@@ -549,7 +549,7 @@ q+=(                "  and srx.row_id = sr.row_id"
             i.high_value = false;
         }
         
-        i.detailed_desc = query.value( 24 ).toString().replace( "]]>", "]]&gt;" ).replace( QByteArray::fromHex("\x1b").data(), "");
+        i.detailed_desc =  query.value( 24 ).toString();
         i.alt_contact = query.value( 25 ).toString();
         i.bugId = query.value( 26 ).toString();
         i.cstNum = query.value( 27 ).toString();
